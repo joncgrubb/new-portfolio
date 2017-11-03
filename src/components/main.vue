ae5e7989ebb4b66e1cf72dc799d72b3d840afc59
joncgrubb <joncgrubbdev@gmail.com>
2017-11-03 14:56:13 -0400

    changed animation timing on transition

--- src/components/main.vue
+++ src/components/main.vue
@@ -106,8 +106,7 @@
   .component-fade-enter-active, .component-fade-leave-active {
   transition: opacity .3s ease;
   }
-  .component-fade-enter, .component-fade-leave-to
-  /* .component-fade-leave-active below version 2.1.8 */ {
+  .component-fade-enter, .component-fade-leave-to {
     opacity: 0;
   }
 
