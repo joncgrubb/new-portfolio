94a9b36199af130fce19baa2a5c5de703726b68e
joncgrubb <joncgrubbdev@gmail.com>
2017-11-18 15:46:47 -0500

    working on revamp of about vue, test blog post

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="about-me">
 
-    <p class="text-center"><b>Web Developer. Coder. Automotive Enthusiast. Awesome Guy.</b></p>
+    <p class="text-center"><b>Web Developer. Philomath. Automotive Enthusiast. Awesome Guy.</b></p>
     <hr>
     <img class="profile-pic" src="../assets/images/jon.jpg">
+    <p class="text-center">Really short blurb about me in general.</p>
+    <div class="container-fluid">  
+      <div class="row">
+        <div class="col-xs-4">
+          <i class="fa fa-file-code-o blurb-icon" aria-hidden="true"></i>
+          <p>Short and sweet info about coding / bootcamp</p>
+        </div>
+        <div class="col-xs-4">
+          <i class="fa fa-wrench blurb-icon" aria-hidden="true"></i>
+          <p>Short and sweet info about cars / mods / kaizen</p>
+        </div>
+        <div class="col-xs-4">
+          <i class="fa fa-mobile blurb-icon" aria-hidden="true"></i>
+          <p>Short and sweet info about phones / roms / dev</p>
+        </div>
+      </div>
+    </div>
+
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a arcu imperdiet, vehicula nisi non, iaculis orci. Sed tincidunt ligula quis erat vestibulum imperdiet. Vestibulum viverra id nisl non mollis. Mauris sagittis lobortis blandit. Aenean eget scelerisque felis, at elementum augue. In venenatis nisi et tortor pulvinar, vitae lacinia arcu dapibus. Proin pretium dolor nulla, laoreet dapibus purus gravida sed. Donec fringilla sit amet elit ut consequat. Suspendisse tempor accumsan tellus vitae vulputate. Ut mollis hendrerit ante, ac scelerisque eros lobortis ut. Aenean euismod id nulla et mollis. In faucibus nisi massa, sit amet viverra sapien vestibulum nec.</p>
     <p>Suspendisse potenti. Etiam finibus, est ac vehicula aliquam, lacus ligula sagittis ante, porta finibus arcu tortor non velit. Donec rhoncus at lorem eget fringilla. Vivamus volutpat tortor sed odio fringilla, vitae pulvinar tortor malesuada. Vestibulum felis turpis, dignissim eu aliquet non, lacinia sit amet lorem. Nam aliquam nisl sed metus congue dictum. Vestibulum justo libero, lacinia id fringilla at, convallis sit amet tortor. Donec et volutpat neque.</p>
 
@@ -34,6 +52,14 @@
 
   .about-me p {
     padding: 3%;
+  }
+  .blurb-icon {
+    display: block;
+    text-align: center;
+    font-size: 40px;
+    color: #777;
+    text-shadow: 1px 1px 2px #ccc;
+    margin-top: 30px;
   }
 
 </style>
---
+++ src/components/posts/2017-09-23-about-me.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="blog">
+    <h1></h1>
+
+    <div id="blog-panel" class="panel panel-default">
+      <div class="panel-body">
+        <p><b>A little about me...</b></p>
+        <hr>
+        <p>Who am I? My name is Jonathan, although most people refer to me by my last name. If I had to describe myself in one word, it would have to be "philomath". From a young age I've had an undending desire to learn, to know how and why things work, and an urge to modify or customize just about everything. I've been in the automotive industry most of my life, with some of my early time spent at the Toyota manufacturing plant in Georgetown, KY. Other than acquiring a great sense of work ethic at a young age from my time there, I also learned about Kaizen. It's become a pretty common buzzword in the professional world for good reason, continuous improvement is a fantastic philosophy. Kaizen is also the reason for my physical and mental <i>need</i> to modify everything in my life.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'blog',
+    data () {
+      return {
+        post: 'Blog post text here?'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  hr {
+    width: 50%;
+  }
+
+  .blog h1 {
+    margin-top: 100px;
+    padding: 5px;
+    width: 25%;
+    min-width: 170px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    box-shadow: .5px .5px 3px #ccc;
+    background-color: white;
+    font-size: 25px;
+  }
+
+  .blog p {
+    padding: 3%;
+  }
+  #blog-panel {
+    box-shadow: .5px .5px 3px #ccc;
+  }
+</style>
