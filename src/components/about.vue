97e7aaa45b1ce7e1be3fa0bd06633b82693c250b
joncgrubb <joncgrubbdev@gmail.com>
2017-11-18 17:50:09 -0500

    working on about vue. still need philomath blurb

--- src/components/about.vue
+++ src/components/about.vue
@@ -4,26 +4,26 @@
     <p class="text-center"><b>Web Developer. Philomath. Automotive Enthusiast. Awesome Guy.</b></p>
     <hr>
     <img class="profile-pic" src="../assets/images/jon.jpg">
-    <p class="text-center">Really short blurb about me in general.</p>
+    <p class="text-center">I have an insatiable desire to learn, modify, and improve everything around me. I've loved technology from a young age and have been in the automotive industry for most of my professional career. From coding and writing scripts for gaming addons and bots, to assisting with development of custom android ROMS with the folks over on the XDA forums, to hiring, training and leading employees in a brand new market for a national top 10 automotive company, my need to learn, improve, and help others do the same has only gotten stronger.</p>
     <div class="container-fluid">  
       <div class="row">
-        <div class="col-xs-4">
+        <div class="col-sm-4">
           <i class="fa fa-code blurb-icon" aria-hidden="true"></i>
-          <p>Short and sweet info about coding / bootcamp</p>
+          <p class="blurb-title"><b>Developer</b></p>
+          <p class="text-center">The Awesome Inc web development bootcamp was a fantastic opportunity for me to break out of my comfort zone in the automotive industry to pursue my dream of becoming an asset in the ever changing technology industry.</p>
         </div>
-        <div class="col-xs-4">
+        <div class="col-sm-4">
           <i class="fa fa-wrench blurb-icon" aria-hidden="true"></i>
-          <p>Short and sweet info about cars / mods / kaizen</p>
+          <p class="blurb-title"><b>Enthusiast</b></p>
+          <p class="text-center">I helped my best friend build a turbocharged 500 wheel horsepower 1.6 liter 1991 Honda CRX that still managed 40+ mpg while not on boost. My current project is a 2006 Acura TSX that I've outfitted with extremely rare Japan-only parts for the car show scene.</p>
         </div>
-        <div class="col-xs-4">
+        <div class="col-sm-4">
           <i class="fa fa-mobile blurb-icon" aria-hidden="true"></i>
-          <p>Short and sweet info about phones / roms / dev</p>
+          <p class="blurb-title"><b>Philomath</b></p>
+          <p class="text-center"></p>
         </div>
       </div>
     </div>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a arcu imperdiet, vehicula nisi non, iaculis orci. Sed tincidunt ligula quis erat vestibulum imperdiet. Vestibulum viverra id nisl non mollis. Mauris sagittis lobortis blandit. Aenean eget scelerisque felis, at elementum augue. In venenatis nisi et tortor pulvinar, vitae lacinia arcu dapibus. Proin pretium dolor nulla, laoreet dapibus purus gravida sed. Donec fringilla sit amet elit ut consequat. Suspendisse tempor accumsan tellus vitae vulputate. Ut mollis hendrerit ante, ac scelerisque eros lobortis ut. Aenean euismod id nulla et mollis. In faucibus nisi massa, sit amet viverra sapien vestibulum nec.</p>
-    <p>Suspendisse potenti. Etiam finibus, est ac vehicula aliquam, lacus ligula sagittis ante, porta finibus arcu tortor non velit. Donec rhoncus at lorem eget fringilla. Vivamus volutpat tortor sed odio fringilla, vitae pulvinar tortor malesuada. Vestibulum felis turpis, dignissim eu aliquet non, lacinia sit amet lorem. Nam aliquam nisl sed metus congue dictum. Vestibulum justo libero, lacinia id fringilla at, convallis sit amet tortor. Donec et volutpat neque.</p>
 
   </div>
 </template>
@@ -47,12 +47,20 @@
     box-shadow: .5px .5px 3px #ccc;
     display: block;
     margin: auto;
+    margin-bottom: 20px;
     border: 1px solid #777;
   }
 
   .about-me p {
     padding: 3%;
   }
+
+  .blurb-title {
+    text-align: center;
+    color: #777;
+    font-size: 16px;
+  }
+
   .blurb-icon {
     display: block;
     text-align: center;
